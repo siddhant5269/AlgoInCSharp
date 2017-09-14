d756b507720af04af784248a1b625685da0eecfb
Siddhant Patra <siddhant5269@gmail.com>
2017-09-14 18:44:37 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpConsoleInVSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
